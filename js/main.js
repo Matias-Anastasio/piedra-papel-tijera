f5bfae32fc133cd1f2d0c10e3a2cdc8203e38da3
Matias Anastasio <matuanastasio@gmail.com>
2022-11-07 11:10:58 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@ const enlPuntajes = document.querySelector("#enlPuntajes");
 const cartelPuntajes = document.querySelector("#cartelPuntajes");
 const cartelBienvenida = document.querySelector("#bienvenida");
 const bloqueo = document.querySelector(".bloquear");
-const tablaScore = {
-    nombre: "",
-    bestof: 0,
-    puntaje: 0,
-    puntajeRival: 0,
-    ganador: false
-};
+const tablaScore = JSON.parse(localStorage.getItem("puntajes")) || [];
+const url = "http://api.giphy.com/v1/gifs/search";
+const key = "&api_key=MV92a77z541WO4lV15mXL6cU5798ztmT&limit=5";
+let urlGif;
+let busqueda = "?q=";
+let q;
+let urlCompleta;
 let puntaje = 0;
 let puntajeRival = 0;
 let alMejorDe;
@@ -36,6 +36,14 @@ const hide = (item) => item.className += " hidden";
 const show = (item) => item = item.classList.remove("hidden");
 
 const generarRival = () => Math.ceil(Math.random() * 10 / 4);
+
+function guardarDatos(nombre, bo, puntaje, puntajeRival){
+    this.nombre = nombre || "Jugador";
+    this.bestof = bo;
+    this.puntaje = puntaje;
+    this.puntajeRival = puntajeRival;
+    this.ganador = puntaje>puntajeRival ? true : false;
+}
 
 const bloquearBtn = () => {
     btnPiedra.setAttribute("disabled", "disabled");
@@ -56,34 +64,20 @@ const desbloquearBtn = () => {
 }
 
 const guardarPuntuacion = () => {
-    if(!nombre==""){
-        tablaScore.nombre = nombre;
-    }
-    else{
-        tablaScore.nombre = "Jugador";
-    }
-    tablaScore.bestof = alMejorDe;
-    tablaScore.puntaje = puntaje;
-    tablaScore.puntajeRival = puntajeRival;
-    if (puntaje > puntajeRival) {
-        tablaScore.ganador = true;
-    }
-    else {
-        tablaScore.ganador = false;
-    }
-    localStorage.setItem(localStorage.length, JSON.stringify(tablaScore));
+    tablaScore.push(new guardarDatos(nombre, alMejorDe, puntaje, puntajeRival));
+    localStorage.setItem("puntajes", JSON.stringify(tablaScore));
 }
 
 const mostrarPuntajes = (cartel) => {
-    for (let i = localStorage.length - 1; i >= 0; i -= 1) {
-        const obj = JSON.parse(localStorage.getItem(i));
+    const puntajesGuardados = JSON.parse(localStorage.getItem("puntajes")) || [];
+    puntajesGuardados.forEach(item => {
         cartelPuntajes.innerHTML += `
-        <div class="${obj.ganador ? "blue" : "red"}">
-        <p>${obj.nombre}: ${obj.puntaje}</p>
-        <p>Rival: ${obj.puntajeRival}</p>
-        <p>Al mejor de: ${obj.bestof}</p>
+        <div class="${item.ganador ? "blue" : "red"}">
+        <p>${item.nombre}: ${item.puntaje}</p>
+        <p>Rival: ${item.puntajeRival}</p>
+        <p>Al mejor de: ${item.bestof}</p>
         </div>`;
-    }
+    });
     cartelPuntajes.innerHTML +=`<button id="btnBorrarPartidas">Borrar Partidas Guardadas</button>`
     const btnBorrarPartidas = document.querySelector("#btnBorrarPartidas");
     let btnPuntajes = document.querySelector("#btnPuntajes");
@@ -104,7 +98,7 @@ const mostrarPuntajes = (cartel) => {
         <button id="btnPuntajes"><span></span><span></span></button>`;
     }
     btnBorrarPartidas.onclick = () =>{
-        localStorage.clear();
+        localStorage.removeItem("puntajes");
         cartelPuntajes.innerHTML=`
         <h2>Partidas Recientes</h2>
         <button id="btnPuntajes"><span></span><span></span></button>`;
@@ -131,16 +125,43 @@ const volverAJugar = () => {
     imgPlayer.src = "";
 }
 
+const nroRandom = () => Math.floor(Math.random() * 5)
+
+const guardarGif = (gif) => {
+    const img = document.querySelector("#imgGif")
+    img.src = gif.data[nroRandom()].images["original"].url;
+}
+
+const obtenerGif = () => {
+    q = puntaje>puntajeRival ? "victory" : "loser";
+    urlCompleta = url + busqueda + q + key;
+    fetch(urlCompleta)
+        .then((respuesta) => {
+            return respuesta.json()
+        })
+        .then((gif) => {
+            guardarGif(gif)
+        })
+        .catch((error) => {
+            console.error("error "+error)
+        })
+        return urlGif;
+}
+
 const finPartida = () => {
     cartelFinal.innerHTML = `
     <h2>Resultado Final</h2>
     <p>${puntaje > puntajeRival ? "¡Ganaste!" : "Perdiste :("}</p>
+    <div class="cartelFinal-gif">
     <div>
-    <p>${nombre=="" ? "Jugador" : nombre}: ${puntaje}</p>
+    <p>${nombre || "Jugador"}: ${puntaje}</p>
     <p>Rival: ${puntajeRival}</p>
     <p>Al mejor de: ${alMejorDe}</p>
     </div>
+    <img id="imgGif" src="">
+    </div>
     <button id="volverAJugar">Volver a Jugar</button>`;
+    obtenerGif();
     show(cartelFinal);
     show(bloqueo);
     const btnVolverAJugar = document.querySelector("#volverAJugar");
@@ -176,8 +197,9 @@ const jugar = (opcion) => {
         document.querySelector("#puntajeRival").textContent = puntajeRival;
     }
     if (puntaje != (Math.ceil(alMejorDe / 2)) && puntajeRival != (Math.ceil(alMejorDe / 2))) {
-        mensaje.textContent += " Proxima ronda en"
+        mensaje.textContent += " Proxima ronda en";
         let i = 3;
+        document.querySelector(".timer").textContent = i;
         const timer = setInterval(() => {
             if (i == 0) {
                 clearInterval(timer);
